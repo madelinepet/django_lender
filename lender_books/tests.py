a54888357bd19c7e131eb3cb67d4ca70af6bbb58
MADELINE PETERS <madelinepet@hotmail.com>
2018-09-19 20:20:10 -0700

    Lab functionality is there, testing works now because I removed the root level double under init. But, some tests fail with an error:django.db.utils.IntegrityError: null value in column user_id violates not-null constraint that I am not sure how to fix. I dropped the db from inside the container and rebuilt as scott did when he got the same error, but it did not help,

--- lender_books/tests.py
+++ lender_books/tests.py
@@ -6,27 +6,39 @@ from django.contrib.auth.models import User
 class TestBookModel(TestCase):
     def setUp(self):
         # add to db through model
-        self.user = User.objects.create(username='test', email='test@example.com')
+        self.user = User.objects.create(username='test', email='test@test.com')
         # set an encrypted password
-        self.user.set_password('hello')
-        self.book = Book.objects.create(title='Feed the Kat', author='Your dog', user=self.user)
-        Book.objects.create(title='Eat some pie')
+        self.user.set_password('1234')
+        self.book = Book.objects.create(
+            title='Feed Kat',
+            author='She likes pie',
+            user=self.user
+        )
+        Book.objects.create(title='Sid tha cat', author='Ruby', user=self.user)
+        Book.objects.create(title='Ruby the kitty', author='Sid', user=self.user)
 
     def test_book_titles(self):
-        self.assertEqual(self.book.title, 'Feed the Kat')
+        self.assertEqual(self.book.title, 'Feed Kat')
+
+    def test_book_detail(self):
+        book = Book.objects.get(title='Sid tha cat')
+
+        self.assertEqual(book.author, 'Ruby')
 
 
 class TestBookViews(TestCase):
     def setUp(self):
+        self.user = User.objects.create(username='test2', email='test2@test.com')
+        self.user.set_password('12345')
         self.request = RequestFactory()
-        self.book1 = Book.objects.create(title='bloop')
-        self.book2 = Book.objects.create(title='derp')
+        self.book_one = Book.objects.create(title='Rooo', author='Sidward')
+        self.book_two = Book.objects.create(title='Sidward', author='Rooo')
 
     def test_book_detail_view(self):
         from .views import books_detail_view
         request = self.request.get('')
         response = books_detail_view(request, f'{self.book_one.id}')
-        self.assertIn(b'bloop', response.content)
+        self.assertIn(b'Sidward', response.content)
 
     def test_book_detail_status(self):
         from .views import books_detail_view
@@ -34,3 +46,5 @@ class TestBookViews(TestCase):
         response = books_detail_view(request, f'{self.book_one.id}')
         self.assertEqual(200, response.status_code)
 
+
+# dir(response) find templates
